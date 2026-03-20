3958a01a45941f7116c40baeeb5343786d36a738
justin <justin1726hernandez2@gmail.com>
2026-03-20 11:20:14 -0400

    full implementation of packet sniffing

---
+++ src/sniffer.cpp
@@ -0,0 +1,87 @@
+#include "sniffer.hpp"
+#include <net/ethernet.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <pcap/pcap.h>
+
+Sniffer::Sniffer(std::string network, std::string bpf_filter, int packet_batch) {
+    connection = network;
+    filter = bpf_filter;
+    count = packet_batch;
+    handler = nullptr;
+}
+
+Sniffer::~Sniffer() {
+    if (handler != nullptr) {
+        pcap_close(handler);
+    }
+}
+
+
+void Sniffer::startSniffing() {
+    char errorBuffer[PCAP_ERRBUF_SIZE];
+    struct bpf_program compiledFilter;
+
+    handler = pcap_open_live(connection.c_str(), 65535, 1, 1000, errorBuffer);
+    if (handler == nullptr) {
+        std::cerr << "open_live failed: " << errorBuffer << "\n";
+        return;
+    }
+
+
+    int compile = pcap_compile(handler, &compiledFilter, filter.c_str(), 1, PCAP_NETMASK_UNKNOWN);
+    if (compile == -1) {
+        std::cout << "compiler failed";
+        return;
+    } 
+
+    int set_filter = pcap_setfilter(handler, &compiledFilter);
+    if (set_filter == -1) {
+        std::cout << "set_filter failed";
+        return;
+    }
+
+
+    pcap_loop(handler, count, packetHandeler, (u_char*) this);
+    pcap_freecode(&compiledFilter);
+    
+
+
+}
+
+void Sniffer::packetHandeler(u_char *args, const pcap_pkthdr *header, const u_char *packet) {
+
+    const struct ether_header *ethernet; /* The ethernet header */
+    const struct iphdr *ip; /* The IP header */
+    const struct tcphdr *tcp; /* The TCP header */
+    const char *payload; /* Packet payload */
+
+
+    Sniffer*  self= (Sniffer*) args;
+
+
+    ethernet = (struct ether_header*)(packet);
+    if (ntohs(ethernet->ether_type) != ETHERTYPE_IP) {
+        return;  // not IPv4, skip it
+    }
+
+    ip = (struct iphdr*)(packet + sizeof(ether_header));
+    if (ip->protocol != IPPROTO_TCP) {
+        return;
+    }
+
+    tcp = (struct tcphdr*)(packet + sizeof(ether_header) + ip->ihl * 4);
+
+    payload = (char*)(packet + sizeof(ether_header) + ip->ihl * 4 + tcp->doff * 4);
+
+
+
+    std::string source_IP = inet_ntoa(*(struct in_addr*)&ip->saddr);
+    std::string dest_IP = inet_ntoa(*(struct in_addr*)&ip->daddr);
+    u_int source_port = ntohs(tcp->source);
+    u_int dest_port = ntohs(tcp->dest);
+    int length = header->len;
+
+    std::cout << "[TCP]" <<source_IP << ":" << source_port << " -> " << dest_IP << ":" << dest_port << "length:" << length << "\n";
+}
